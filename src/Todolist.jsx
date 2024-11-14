49978fd4c4a7ade83ccceae056044bc1dd5bc017
ANANTTIWARI01 <bhaukalbaba45@gmail.com>
2024-11-14 16:37:48 +0530

    first commit

--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -1,76 +1,97 @@
+
 // import React from 'react'
-import { useState } from 'react';
+import { MdDelete } from "react-icons/md";
+import { MdModeEditOutline } from "react-icons/md";
+import { useState } from "react";
 
 function Todolist() {
+  const [input, setInput] = useState("");
+  const [Tasks, setTask] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [changedObjectId, setChangedObjectId] = useState(null);
+  // const [checkBox, setCheckBox]=useState(false)
 
-  const [input, setInput] = useState("")
-  const [Tasks, setTask] =  useState([])
-  let flag= false;
-  flag
-  
-  let changedObject=null;
-  let isEditing =null;
-function trial(){
-  // console.log(tre)
-  
-  if(isEditing){
-    isEditing = input
-    Tasks.map((obj)=>
-      {
-        if(obj.task ===changedObject )
-          { obj.task = input
+  // let flag = false;
 
-          }
-          return obj
-        })
-        isEditing=null
-        flag=false
-      }
-      else{
-        addTask()
+  // let changedObjectId = null;
+  // let isEditing = false;
+
+  function trial() {
+    // console.log(tre)
+
+    if (isEditing) {
+      // Tasks.map((obj) => {
+      //   if (obj.id === changedObjectId) {
+      //     obj.task = input;
+      //   }
+      //   return obj;
+      // });
+      setTask(
+        Tasks.map((obj) =>
+          obj.id === changedObjectId ? { ...obj, task: input } : obj
+        )
+      );
+
+      setIsEditing(false);
+      setChangedObjectId(null);
+    } else {
+      addTask();
+    }
   }
-}
-
 
   function addTask() {
-    const Obj = { id: Date.now(), task: input }
-    setTask([...Tasks, Obj])
-    setInput("")
+    const Obj = { id: Date.now(), task: input,isChecked:false };
+    setTask([...Tasks, Obj]);
+    setInput("");
   }
 
   function Delete(id) {
-     setTask( Tasks.filter(obj => obj.id !== id))
+    setTask(Tasks.filter((obj) => obj.id !== id));
   }
 
-
-  function Edit(task) {
-    setInput(task)
-    changedObject  = task;
-    isEditing=input
-    
-    flag =true
-
+  function Edit(obj) {
+    setInput(obj.task);
+    setChangedObjectId(obj.id);
+    setIsEditing(true);
+    // flag = true;
   }
 
+  function toggleCheckbox(id) {
+    setTask(Tasks.map((obj) =>
+      obj.id === id ? { ...obj, isChecked: !obj.isChecked } : obj
+    ));
+  }
 
   return (
     <>
-      <input type="text" placeholder='ENTER TASK' value={input} onChange={(e) => setInput(e.target.value)} />
-      <button onClick={trial}>Add Task</button>
+      <input
+        type="text"
+        placeholder="ENTER TASK"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={trial}>{isEditing ? "Update Task" : "Add Task"}</button>
       <ul>
         {Tasks.map((obj) => {
-          return <li key={obj.id}>{obj.task}
-
-            <button onClick={()=> Delete(obj.id)}>Delete
-            </button>
-            <button onClick={()=> Edit(obj.task)}>Edit
-            </button>
-          </li>
+          return (
+            <li key={obj.id}>
+               <input
+              type="checkbox"
+              checked={obj.isChecked}
+              onChange={()=>toggleCheckbox(obj.id)} 
+            />
+              {obj.task}
+              <MdDelete onClick={() => Delete(obj.id)} 
+                style={{ pointerEvents: obj.isChecked ? 'none' : 'auto', opacity: obj.isChecked ? 0.5 : 1 }}/>
+              <MdModeEditOutline onClick={() => Edit(obj)}
+              style={{ pointerEvents: obj.isChecked ? 'none' : 'auto', opacity: obj.isChecked ? 0.5 : 1 }}
+               />
+            </li>
+          );
         })}
       </ul>
     </>
-
-  )
+  );
 }
 
-export default Todolist
+export default Todolist;
